03015dcc4f1cf2c2c1156721d09d4eff8a410917
Tyler Gillson <ty@liveoutthere.com>
2012-11-01 23:31:41 -0600

    Fixed HH Loadfile

--- Seasonal Data Loads/F12/HHF12.sql
+++ Seasonal Data Loads/F12/HHF12.sql
@@ -39,7 +39,7 @@ CREATE TABLE [dbo].[tbl_LoadFile_F12_HH](
 	[vendor_product_id] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[vendor_color_code] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[vendor_size_code] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[season] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_tbl_LoadFile_F12_HH_season]  DEFAULT (N'FW12A'),
+	[season] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_tbl_LoadFile_F12_HH_season]  DEFAULT (N'FW12 ASAP'),
 	[short_description] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[description] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[features] [nvarchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -96,11 +96,12 @@ INSERT INTO tbl_LoadFile_F12_HH (
 		[type],
 		[image],
 		image_label,
-		url_key
+		url_key,
+		backorders
 )
 SELECT  dbo.getMagentoSimpleSKU('FW12A-HH', LEFT(a.SKU,5), SUBSTRING(a.SKU,7,3), dbo.getHHSize(SUBSTRING(a.SKU,11,7))) AS sku,
 		LEFT(a.SKU,5) AS vendor_product_id,
-		dbo.getHHName(a.StyleName, a.Gender) AS name,
+		dbo.getHHName(a.StyleName, '') AS name,
 		CASE WHEN a.Gender = 'Womens' THEN 'Women' WHEN a.Gender = 'Mens' THEN 'Men' WHEN a.Gender = 'Kid' THEN 'Boy|Girl' WHEN a.Gender = 'Junior' THEN 'Boy|Girl' WHEN a.Gender = 'Unisex' THEN 'Men|Women' END AS gender,
 		REPLACE(dbo.ProperCase(dbo.getHHColorName(a.ColorName)),'Hh ','HH ') AS choose_color,
 		dbo.getHHSize(SUBSTRING(a.SKU,11,7)) AS choose_size,
@@ -113,8 +114,9 @@ SELECT  dbo.getMagentoSimpleSKU('FW12A-HH', LEFT(a.SKU,5), SUBSTRING(a.SKU,7,3),
 		'simple' AS type,
 		dbo.getHHImage(a.SKU) AS image,
 		REPLACE(dbo.ProperCase(dbo.getHHColorName(a.ColorName)),'Hh ','HH ') AS image_label,
-		dbo.getUrlKey(dbo.getHHName(a.StyleName, a.Gender), 'Helly Hansen', a.ColorName + ' - ' + SUBSTRING(a.SKU,11,5)) + '-fw12a' AS url_key
-FROM tbl_RawData_F12_HH AS a
+		dbo.getUrlKey(dbo.getHHName(a.StyleName, a.Gender), 'Helly Hansen', a.ColorName + ' - ' + SUBSTRING(a.SKU,11,5)) + '-fw12a' AS url_key,
+		1 AS backorders
+FROM tbl_RawData_F12_HH_UPC2 AS a
 
 INSERT INTO tbl_LoadFile_F12_HH (
 	sku,
@@ -130,14 +132,15 @@ INSERT INTO tbl_LoadFile_F12_HH (
 	url_key,
 	visibility,
 	merchandise_priority,
-	manage_stock
+	manage_stock,
+	backorders
 )
 SELECT DISTINCT
 	    dbo.getMagentoConfigurableSKU('FW12A-HH', LEFT(a.SKU,5)) AS sku,
 		'choose_color,choose_size' AS configurable_attributes,
 		LEFT(a.SKU,5) AS vendor_product_id,
 		'Uncategorized' AS categories,
-		dbo.getHHName(a.StyleName, a.Gender) AS name,
+		dbo.getHHName(a.StyleName, '') AS name,
 		CASE WHEN a.Gender = 'Womens' THEN 'Women' WHEN a.Gender = 'Mens' THEN 'Men' WHEN a.Gender = 'Kid' THEN 'Boy|Girl' WHEN a.Gender = 'Junior' THEN 'Boy|Girl' WHEN a.Gender = 'Unisex' THEN 'Men|Women' END AS gender,
 		(SELECT MAX(price) FROM tbl_LoadFile_F12_HH WHERE vendor_product_id = LEFT(a.SKU,5)) AS price,
 		(SELECT MAX(cost) FROM tbl_LoadFile_F12_HH WHERE vendor_product_id = LEFT(a.SKU,5)) AS cost,
@@ -146,8 +149,9 @@ SELECT DISTINCT
 		dbo.getUrlKey(dbo.getHHName(a.StyleName, a.Gender), 'Helly Hansen', '') + '-fw12a' AS url_key,
 		'Catalog, Search' AS visibility,
 		'Z' AS merchandise_priority,
-		0 AS manage_stock
-FROM tbl_RawData_F12_HH AS a
+		0 AS manage_stock,
+		NULL AS backorders
+FROM tbl_RawData_F12_HH_UPC2 AS a
 
 UPDATE a SET
 	description = dbo.ProperCase(dbo.toHTMLEntities(REPLACE((SELECT DISTINCT TOP 1 [Product Statement] FROM tbl_RawData_F12_HH WHERE LEFT(SKU,5) LIKE a.vendor_product_id),'#N/A',''))),
@@ -166,39 +170,34 @@ WHERE type = 'configurable'
 
 UPDATE tbl_LoadFile_F12_HH SET thumbnail = '+' + image, small_image = '+' + image WHERE image IS NOT NULL
 UPDATE tbl_LoadFile_F12_HH SET image = '+' + image WHERE image IS NOT NULL
-
-SELECT * FROM tbl_LoadFile_F12_HH WHERE image IS NULL
 GO
-/*
+
 CREATE VIEW [dbo].[view_LoadFile_F12_HH]
 AS
-SELECT  '"store"' AS store, '"websites"' AS websites, '"type"' AS type, '"sku"' AS sku, '"name"' AS name, '"categories"' AS categories, '"attribute_set"' AS attribute_set, 
+SELECT  '"store"' AS store, '"websites"' AS websites, '"type"' AS type, '"sku"' AS sku, '"name"' AS name, '"attribute_set"' AS attribute_set, 
         '"configurable_attributes"' AS configurable_attributes, '"has_options"' AS has_options, '"price"' AS price, '"cost"' AS cost, '"status"' AS status, '"tax_class_id"' AS tax_class_id, '"department"' AS department, 
         '"visibility"' AS visibility, '"image"' AS image, '"image_label"' AS image_label, '"small_image"' AS small_image, '"thumbnail"' AS thumbnail, '"media_gallery"' AS media_gallery, 
         '"choose_color"' AS choose_color, '"choose_size"' AS choose_size, '"vendor_sku"' AS vendor_sku, '"vendor_product_id"' AS vendor_product_id, '"vendor_color_code"' AS vendor_color_code, 
-        '"vendor_size_code"' AS vendor_size_code, '"season"' AS season, '"short_description"' AS short_description, '"description"' AS description, '"features"' AS features, '"activities"' AS activities, '"weather"' AS weather, '"layering"' AS layering, '"care_instructions"' AS care_instructions,
-        '"fabric"' AS fabric, '"fit"' AS fit, '"volume"' AS volume, '"manufacturer"' AS manufacturer, '"qty"' AS qty, '"is_in_stock"' AS is_in_stock, '"simples_skus"' AS simples_skus, '"url_key"' AS url_key,
-        '"super_attribute_pricing"' AS super_attribute_pricing, '"videos"' AS videos, '"hs_tariff_id"' AS hs_tariff_id, '"origin"' AS origin, '"weight"' AS weight, '"us_skus"' AS us_skus, '"cs_skus"' AS cs_skus, '"xre_skus"' AS xre_skus, '"merchandise_priority"' AS merchandise_priority, '"backorders"' AS backorders, '"manage_stock"' AS manage_stock
+        '"vendor_size_code"' AS vendor_size_code, '"season"' AS season, '"short_description"' AS short_description, '"description"' AS description, '"features"' AS features,
+        '"fabric"' AS fabric, '"manufacturer"' AS manufacturer, '"simples_skus"' AS simples_skus, '"url_key"' AS url_key,
+        '"merchandise_priority"' AS merchandise_priority, '"backorders"' AS backorders, '"manage_stock"' AS manage_stock
 UNION ALL
-SELECT  '"' + RTRIM(LTRIM(REPLACE(a.store,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.websites,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.type,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.sku,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.name,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.categories,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.attribute_set,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.configurable_attributes,'"','""'))) + '"',
+SELECT  '"' + RTRIM(LTRIM(REPLACE(a.store,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.websites,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.type,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.sku,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.name,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.attribute_set,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.configurable_attributes,'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.has_options,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.price,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.cost,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.status,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.tax_class_id,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.gender,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.visibility,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.image,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.image_label,'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.small_image,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.thumbnail,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.media_gallery,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.choose_color,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.choose_size,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_sku,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_product_id,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.vendor_color_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_size_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.season,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a. short_description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.features,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.activities,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weather,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.layering,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.care_instructions,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.fabric,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.fit,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.volume,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manufacturer,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.qty,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.is_in_stock,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.simples_skus,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.url_key,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.super_attribute_pricing,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.videos,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.hs_tariff,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.origin,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weight,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.us_skus,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.cs_skus,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.xre_skus,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.merchandise_priority,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.backorders,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manage_stock,'"','""'))) + '"' 
-FROM dbo.tbl_LoadFile_F12_HH AS a WHERE a.image IS NOT NULL
+		'"' + RTRIM(LTRIM(REPLACE(a.vendor_color_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_size_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.season,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a. short_description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.features,'"','""'))) + '"',
+		'"' + RTRIM(LTRIM(REPLACE(a.fabric,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manufacturer,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.simples_skus,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.url_key,'"','""'))) + '"',
+		'"' + RTRIM(LTRIM(REPLACE(a.merchandise_priority,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.backorders,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manage_stock,'"','""'))) + '"' 
+FROM dbo.tbl_LoadFile_F12_HH AS a --WHERE a.image IS NOT NULL
 GO
 
-DECLARE @sql varchar(1024)
-SELECT @sql = 'bcp "SELECT * FROM LOT_Inventory.dbo.view_LoadFile_F12_HH" queryout "C:\Data\Shared\HH.csv" -w -t , -T -S ' + @@servername
-EXEC master..xp_cmdshell @sql
+DECLARE @sql varchar(1024), @Filename varchar(255)
+SET @Filename = 'HH.csv'
+SELECT @sql = 'bcp "SELECT * FROM LOT_Inventory.dbo.view_LoadFile_F12_HH" queryout "C:\Data\Shared\' + @Filename + '" -w -t , -T -S ' + @@servername
+EXEC master..xp_cmdshell @sql, NO_OUTPUT
+
+-- If this doesn't work, it's because the file is too large and wget isn't waiting long enough before returning control to sproc_run_MAGMI. In that case, just open cmd from the Start Menu > Run, and paste this:
+-- wget "http://www.liveoutthere.com/utility_o23tsgf/magmi/web/magmi_run.php?mode=create&logfile=missing-skus.log&profile=Missing%20SKUs&engine=magmi_productimportengine:Magmi_ProductImportEngine"wget "http://www.liveoutthere.com/utility_o23tsgf/magmi/web/magmi_run.php?mode=create&logfile=missing-skus.log&profile=Missing%20SKUs&engine=magmi_productimportengine:Magmi_ProductImportEngine"
+EXEC sproc_run_MAGMI @Filename, 0
 
 DROP VIEW view_LoadFile_F12_HH
-
-/* Next steps?
-
-1. Use Magmi to load ONLY NEW SKUs so we don't overwrite existing categories to Uncategorized
-2. Then move all new products into correct categories
-3. Then use Magmi to load ONLY UPDATED skus but don't include the categories column so we don't overwrite it
-
-*/*/

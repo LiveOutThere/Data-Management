0a27e37d63ab551d6cda642e489f810d79831fa4
Tyler Gillson <ty@liveoutthere.com>
2013-01-23 12:26:17 -0700

    Moved photos test to the top of the Test Script

--- Tools/Data Load Test Script.sql
+++ Tools/Data Load Test Script.sql
@@ -1,13 +1,31 @@
 DECLARE @output varchar(MAX), @count nvarchar(3)
 
-PRINT 'Patagonia'
+PRINT 'Lole'
+
+-- Simples Without Photos test
+PRINT ''
+SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
+(SELECT DISTINCT vendor_product_id + '-' + vendor_color_code  + ' (' + choose_color + ')' AS name FROM tbl_LoadFile_SS13_LOLE WHERE type = 'simple' AND (image = '' OR image IS NULL)) AS x
+
+SET @count = (SELECT COUNT(DISTINCT vendor_product_id + ' ' + choose_color) FROM tbl_LoadFile_SS13_LOLE WHERE type = 'simple' AND (image = '' OR image IS NULL))
+
+IF @count <> 0 BEGIN
+	PRINT 'Failed Simples Without Photos test'
+	PRINT '	There are ' + @count + ' simple products that don''t have images:'
+	PRINT '		' + @output
+	SET @output = ''
+END
+ELSE BEGIN
+	PRINT 'Passed Simples Without Photos test'
+END
+SET @output = NULL
 
 -- Categories test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND categories IS NULL) AS x
+(SELECT name FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND categories IS NULL) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND categories IS NULL)
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND categories IS NULL)
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Categories test'
@@ -22,7 +40,7 @@ SET @output = NULL
 -- Duplicate SKUs test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + sku FROM
-(SELECT a.sku AS sku FROM tbl_LoadFile_SS13_PAT AS a INNER JOIN tbl_LoadFile_SS13_PAT AS b
+(SELECT a.sku AS sku FROM tbl_LoadFile_SS13_LOLE AS a INNER JOIN tbl_LoadFile_SS13_LOLE AS b
  ON a.sku = b.sku
  WHERE a.id <> b.id) AS x
  
@@ -38,13 +56,13 @@ END
 
 /* Alterate (better) method of finding duplicates:
    SELECT sku FROM
-   tbl_LoadFile_SS13_PAT AS a
-   WHERE (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE sku = a.sku) > 1 */
+   tbl_LoadFile_SS13_LOLE AS a
+   WHERE (SELECT COUNT(*) FROM tbl_LoadFile_SS13_LOLE WHERE sku = a.sku) > 1 */
 	
 -- Associated Simple/Configurables test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + sku FROM
-(SELECT a.sku AS sku FROM tbl_LoadFile_SS13_PAT AS a LEFT JOIN tbl_LoadFile_SS13_PAT AS b
+(SELECT a.sku AS sku FROM tbl_LoadFile_SS13_LOLE AS a LEFT JOIN tbl_LoadFile_SS13_LOLE AS b
  ON b.type = 'configurable' AND b.simples_skus LIKE '%' + a.sku + '%'
  WHERE a.type = 'simple' AND b.sku IS NULL) AS x
  
@@ -61,9 +79,9 @@ END
 -- Prices test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + vendor_product_id FROM
-(SELECT vendor_product_id FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND CAST(price AS float) <= 0 OR price IS NULL) AS x
+(SELECT vendor_product_id FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND CAST(price AS float) <= 0 OR price IS NULL) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND CAST(price AS float) <= 0 OR price IS NULL)
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND CAST(price AS float) <= 0 OR price IS NULL)
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Prices test'
@@ -79,9 +97,9 @@ SET @output = NULL
 -- Descriptions test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (description IS NULL OR description = '')) AS x
+(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND (description IS NULL OR description = '')) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (description IS NULL OR description = ''))
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND (description IS NULL OR description = ''))
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Descriptions test'
@@ -97,9 +115,9 @@ SET @output = NULL
 -- Features test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (features IS NULL OR features = '')) AS x
+(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND (features IS NULL OR features = '')) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (features IS NULL OR features = ''))
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_LOLE WHERE type = 'configurable' AND (features IS NULL OR features = ''))
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Features test'
@@ -112,20 +130,3 @@ ELSE BEGIN
 END
 SET @output = NULL
 
--- Simples Without Photos test
-PRINT ''
-SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT DISTINCT vendor_product_id + '-' + vendor_color_code  + ' (' + choose_color + ')' AS name FROM tbl_LoadFile_SS13_PAT WHERE type = 'simple' AND (image = '' OR image IS NULL)) AS x
-
-SET @count = (SELECT COUNT(DISTINCT vendor_product_id + ' ' + choose_color) FROM tbl_LoadFile_SS13_PAT WHERE type = 'simple' AND (image = '' OR image IS NULL))
-
-IF @count <> 0 BEGIN
-	PRINT 'Failed Simples Without Photos test'
-	PRINT '	There are ' + @count + ' simple products that don''t have images:'
-	PRINT '		' + @output
-	SET @output = ''
-END
-ELSE BEGIN
-	PRINT 'Passed Simples Without Photos test'
-END
-SET @output = NULL

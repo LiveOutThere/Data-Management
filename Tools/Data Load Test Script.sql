e19acbf4f43f55cff4b4bb5fcab819f21961f50a
Tyler Gillson <ty@liveoutthere.com>
2013-01-22 17:35:24 -0700

    Testing

--- Tools/Data Load Test Script.sql
+++ Tools/Data Load Test Script.sql
@@ -1,13 +1,13 @@
 DECLARE @output varchar(MAX), @count nvarchar(3)
 
-PRINT 'Merrell'
+PRINT 'Patagonia'
 
 -- Categories test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND categories IS NULL) AS x
+(SELECT name FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND categories IS NULL) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND categories IS NULL)
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND categories IS NULL)
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Categories test'
@@ -22,7 +22,7 @@ SET @output = NULL
 -- Duplicate SKUs test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + sku FROM
-(SELECT a.sku AS sku FROM tbl_LoadFile_F12_MER AS a INNER JOIN tbl_LoadFile_F12_MER AS b
+(SELECT a.sku AS sku FROM tbl_LoadFile_SS13_PAT AS a INNER JOIN tbl_LoadFile_SS13_PAT AS b
  ON a.sku = b.sku
  WHERE a.id <> b.id) AS x
  
@@ -38,13 +38,13 @@ END
 
 /* Alterate (better) method of finding duplicates:
    SELECT sku FROM
-   tbl_LoadFile_F12_MER AS a
-   WHERE (SELECT COUNT(*) FROM tbl_LoadFile_F12_MER WHERE sku = a.sku) > 1 */
+   tbl_LoadFile_SS13_PAT AS a
+   WHERE (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE sku = a.sku) > 1 */
 	
 -- Associated Simple/Configurables test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + sku FROM
-(SELECT a.sku AS sku FROM tbl_LoadFile_F12_MER AS a LEFT JOIN tbl_LoadFile_F12_MER AS b
+(SELECT a.sku AS sku FROM tbl_LoadFile_SS13_PAT AS a LEFT JOIN tbl_LoadFile_SS13_PAT AS b
  ON b.type = 'configurable' AND b.simples_skus LIKE '%' + a.sku + '%'
  WHERE a.type = 'simple' AND b.sku IS NULL) AS x
  
@@ -58,29 +58,12 @@ ELSE BEGIN
 	PRINT 'Passed Associated Simple/Configurables test'
 END
 
--- Configurable Images test
-PRINT ''
-SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND (image IS NULL or image_label IS NULL)) AS x
-
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND (image IS NULL or image_label IS NULL))
-
-IF @count <> 0 BEGIN
-	PRINT 'Failed Configurable Images test'
-	PRINT '	There are ' + @count + ' styles missing a default image or image label:'
-	PRINT '		' + @output
-END
-ELSE BEGIN
-	PRINT 'Passed Configurable Images test'
-END
-SET @output = NULL
-
 -- Prices test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + vendor_product_id FROM
-(SELECT vendor_product_id FROM tbl_LoadFile_F12_MER WHERE CAST(price AS float) <= 0 OR price IS NULL) AS x
+(SELECT vendor_product_id FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND CAST(price AS float) <= 0 OR price IS NULL) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_F12_MER WHERE CAST(price AS float) <= 0 OR price IS NULL)
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND CAST(price AS float) <= 0 OR price IS NULL)
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Prices test'
@@ -96,13 +79,13 @@ SET @output = NULL
 -- Descriptions test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND (short_description IS NULL OR short_description = '')) AS x
+(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (description IS NULL OR description = '')) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND (short_description IS NULL OR short_description = ''))
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (description IS NULL OR description = ''))
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Descriptions test'
-	PRINT '	There are ' + @count + ' styles missing a short description:'
+	PRINT '	There are ' + @count + ' styles missing a description:'
 	PRINT '		' + @output
 	SET @output = ''
 END
@@ -111,30 +94,30 @@ ELSE BEGIN
 END
 SET @output = NULL
 
--- Media Gallery test
+-- Features test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT name FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND media_gallery LIKE '%' + image + '%') AS x
+(SELECT name + ' (' + vendor_product_id + ')' AS name FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (features IS NULL OR features = '')) AS x
 
-SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_F12_MER WHERE type = 'configurable' AND media_gallery LIKE '%' + image + '%')
+SET @count = (SELECT COUNT(*) FROM tbl_LoadFile_SS13_PAT WHERE type = 'configurable' AND (features IS NULL OR features = ''))
 
 IF @count <> 0 BEGIN
-	PRINT 'Failed Media Gallery formatting test'
-	PRINT '	There are ' + @count + ' configurable styles that have the image included in the media gallery:'
+	PRINT 'Failed Features test'
+	PRINT '	There are ' + @count + ' styles missing features:'
 	PRINT '		' + @output
 	SET @output = ''
 END
 ELSE BEGIN
-	PRINT 'Passed Media Gallery test'
+	PRINT 'Passed Features test'
 END
 SET @output = NULL
 
 -- Simples Without Photos test
 PRINT ''
 SELECT @output = COALESCE(@output + CHAR(13) + '		', '') + name FROM
-(SELECT DISTINCT vendor_product_id + '-' + vendor_color_code  + ' (' + choose_color + ')' AS name FROM tbl_LoadFile_F12_MER WHERE type = 'simple' AND (image = '' OR image IS NULL)) AS x
+(SELECT DISTINCT vendor_product_id + '-' + vendor_color_code  + ' (' + choose_color + ')' AS name FROM tbl_LoadFile_SS13_PAT WHERE type = 'simple' AND (image = '' OR image IS NULL)) AS x
 
-SET @count = (SELECT COUNT(DISTINCT vendor_product_id + ' ' + choose_color) FROM tbl_LoadFile_F12_MER WHERE type = 'simple' AND (image = '' OR image IS NULL))
+SET @count = (SELECT COUNT(DISTINCT vendor_product_id + ' ' + choose_color) FROM tbl_LoadFile_SS13_PAT WHERE type = 'simple' AND (image = '' OR image IS NULL))
 
 IF @count <> 0 BEGIN
 	PRINT 'Failed Simples Without Photos test'

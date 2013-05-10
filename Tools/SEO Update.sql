b2cf9ff29598e0eb7a4d60f1b10b2d1bea9bcef9
Tyler Gillson <ty@liveoutthere.com>
2013-05-10 16:48:48 -0600

    modified SEO UPDT Query and added new Incomplete POs report for Aidan

--- Tools/SEO Update.sql
+++ Tools/SEO Update.sql
@@ -5,26 +5,26 @@ UPDATE a SET
 	meta_description = ISNULL((SELECT meta_description FROM tbl_SEO_Staging WHERE sku = a.sku),a.meta_description),
 	description = ISNULL((SELECT description FROM tbl_SEO_Staging WHERE sku = a.sku),a.description),
 	videos = ISNULL((SELECT videos FROM tbl_SEO_Staging WHERE sku = a.sku),a.videos)
-FROM tbl_LoadFile_SS13_ARC AS a
+FROM tbl_LoadFile_SS13_PAT AS a
 WHERE a.type = 'configurable'
 GO
 
 --next, export the new data to .csv, stripping line breaks
 
-CREATE VIEW [dbo].[view_ARC_SEO_Update]
+CREATE VIEW [dbo].[view_PAT_SEO_Update]
 AS
 SELECT  '"sku"' AS sku, '"meta_description"' AS meta_description, '"description"' AS description, '"videos"' AS videos
 UNION ALL
 SELECT  '"' + RTRIM(LTRIM(REPLACE(a.sku,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(a. meta_description,'"','""'),CHAR(10),''),CHAR(13),''))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(REPLACE(a.description,'"','""'),CHAR(10),''),CHAR(13),''))) + '"','"' + RTRIM(LTRIM(REPLACE(a.videos,'"','""'))) + '"'
-FROM dbo.tbl_LoadFile_SS13_ARC AS a
+FROM dbo.tbl_LoadFile_SS13_PAT AS a
 INNER JOIN tbl_SEO_Staging AS b
 ON a.sku = b.sku
 GO
 
 DECLARE @sql varchar(1024)
-SELECT @sql = 'bcp "SELECT * FROM LOT_Inventory.dbo.view_ARC_SEO_Update" queryout "C:\Data\Shared\SS13ARCSEOUPDT.csv" -w -t , -T -S ' + @@servername
+SELECT @sql = 'bcp "SELECT * FROM LOT_Inventory.dbo.view_PAT_SEO_Update" queryout "C:\Data\Shared\SS13PATSEOUPDT.csv" -w -t , -T -S ' + @@servername
 EXEC master..xp_cmdshell @sql
 
-DROP VIEW view_ARC_SEO_Update
+DROP VIEW view_PAT_SEO_Update
 
 --lastly, pump resulting .csv through MAGMI. Done!

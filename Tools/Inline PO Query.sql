e1c0cae529dec469d304f05f95fdee96f73352e6
Tyler Gillson <ty@liveoutthere.com>
2013-08-20 12:04:24 -0600

    Updated Inline PO Query for Fall 13

--- Tools/Inline PO Query.sql
+++ Tools/Inline PO Query.sql
@@ -8,32 +8,124 @@ SELECT  '"store"' AS store, '"websites"' AS websites, '"type"' AS type, '"sku"' 
         '"fabric"' AS fabric, '"fit"' AS fit, '"volume"' AS volume, '"manufacturer"' AS manufacturer, '"qty"' AS qty, '"is_in_stock"' AS is_in_stock, '"simples_skus"' AS simples_skus, '"url_key"' AS url_key,
         '"videos"' AS videos, '"weight"' AS weight, '"merchandise_priority"' AS merchandise_priority, '"backorders"' AS backorders, '"manage_stock"' AS manage_stock, '"never_backorder"' AS never_backorder, '"use_config_manage_stock"' AS use_config_manage_stock, '"use_config_backorders"' AS use_config_backorders, '"meta_title"' AS meta_title
 UNION ALL
-SELECT  '"' + RTRIM(LTRIM(REPLACE(a.store,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.websites,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.type,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.sku,'SS13A','SS13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.name,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.categories,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.attribute_set,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.configurable_attributes,'"','""'))) + '"',
+SELECT  '"' + RTRIM(LTRIM(REPLACE(a.store,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.websites,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.type,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.sku,'FW13A','FW13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.name,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.categories,'"','""'))) + '"','"default"','"' + RTRIM(LTRIM(REPLACE(a.configurable_attributes,'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.has_options,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.price,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.cost,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.status,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.tax_class_id,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.department,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.visibility,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.image,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.image_label,'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.small_image,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.thumbnail,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.choose_color,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.choose_size,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_sku,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_product_id,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.vendor_color_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_size_code,'"','""'))) + '"','"SS13 Inline"','"' + RTRIM(LTRIM(REPLACE(a. short_description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.features,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.activities,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weather,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.layering,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.care_instructions,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.fabric,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.fit,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.volume,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manufacturer,'"','""'))) + '"',/*qty:*/'"0"',/*is_in_stock:*/'"0"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.simples_skus,'SS13A','SS13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.url_key,'ss13a','ss13i'),'"','""'))) + '"',
+		'"' + RTRIM(LTRIM(REPLACE(a.vendor_color_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_size_code,'"','""'))) + '"','"FW13 Inline"','"' + RTRIM(LTRIM(REPLACE(a. short_description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.features,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.activities,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weather,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.layering,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.care_instructions,'"','""'))) + '"',
+		'"' + RTRIM(LTRIM(REPLACE(a.fabric,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.fit,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.volume,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manufacturer,'"','""'))) + '"',/*qty:*/'"0"',/*is_in_stock:*/'"0"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.simples_skus,'FW13A','FW13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.url_key,'fw13a','fw13i'),'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.videos,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weight,'"','""'))) + '"',/*merchandise_priority:*/'""',/*backorders:*/'"0"',/*manage_stock:*/'"1"',/*never_backorder:*/'"1"',/*use_config_manage_stock:*/'"1"',/*use_config_backorders:*/'"1"','"' + RTRIM(LTRIM(REPLACE(a.meta_title,'"','""'))) + '"'
-FROM dbo.tbl_LoadFile_SS13_OSP AS a
-WHERE type = 'simple' AND vendor_product_id + ',' + choose_color IN()
+FROM dbo.tbl_LoadFile_FW13_TNF AS a
+WHERE type = 'simple' AND /*vendor_product_id + '-' + choose_color + '-' + choose_size*/vendor_sku IN('808390589002',
+'808390589019',
+'808390589033',
+'808390589040',
+'808390596307',
+'808390596314',
+'808390596338',
+'808390596345',
+'808390596376',
+'808390596444',
+'808390596451',
+'808390596475',
+'808390596482',
+'808390596499',
+'887682252541',
+'887682252565',
+'887682252572',
+'887682252589',
+'887682252619',
+'887682252633',
+'887682252640',
+'887682252657',
+'766182368573',
+'766182368610',
+'766182368634',
+'766182368641',
+'766182368658',
+'766182368849',
+'766182368894',
+'766182368917',
+'766182368924',
+'766182368979',
+'27906714282',
+'27906714299',
+'27906714305',
+'27906714312',
+'27906714329',
+'648335003541',
+'648335003558',
+'648335003565',
+'648335003572',
+'648335003589',
+'881862500880',
+'881862500903',
+'881862500910',
+'881862500927',
+'881862500941',
+'881862501023',
+'881862501047',
+'881862501054',
+'887040783403',
+'887040783427',
+'887040783441',
+'887040783458',
+'885928317573',
+'885928317580',
+'885928317597',
+'885928317603',
+'808390995872',
+'808390995933',
+'808390995971',
+'808390996008',
+'808390996046',
+'766182238647',
+'766182238661',
+'766182238678',
+'766182238685',
+'766182239378',
+'766182239392',
+'766182239408',
+'766182239415',
+'32546101750',
+'32546101767',
+'32546101774',
+'32546101781',
+'881862936993',
+'881862937013',
+'881862937020',
+'881862937037',
+'881862937051',
+'881862937129',
+'881862937136',
+'881862937143')
 UNION ALL
-SELECT  '"' + RTRIM(LTRIM(REPLACE(a.store,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.websites,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.type,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.sku,'SS13A','SS13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.name,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.categories,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.attribute_set,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.configurable_attributes,'"','""'))) + '"',
+SELECT  '"' + RTRIM(LTRIM(REPLACE(a.store,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.websites,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.type,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.sku,'FW13A','FW13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.name,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.categories,'"','""'))) + '"','"default"','"' + RTRIM(LTRIM(REPLACE(a.configurable_attributes,'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.has_options,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.price,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.cost,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.status,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.tax_class_id,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.department,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.visibility,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.image,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.image_label,'"','""'))) + '"',
 		'"' + RTRIM(LTRIM(REPLACE(a.small_image,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.thumbnail,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.choose_color,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.choose_size,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_sku,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_product_id,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.vendor_color_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_size_code,'"','""'))) + '"','"SS13 Inline"','"' + RTRIM(LTRIM(REPLACE(a. short_description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.features,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.activities,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weather,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.layering,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.care_instructions,'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.fabric,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.fit,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.volume,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manufacturer,'"','""'))) + '"',/*qty:*/'""',/*is_in_stock:*/'""','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.simples_skus,'SS13A','SS13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.url_key,'ss13a','ss13i'),'"','""'))) + '"',
-		'"' + RTRIM(LTRIM(REPLACE(a.videos,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weight,'"','""'))) + '"',/*merchandise_priority:*/'"Z"',/*backorders:*/'"0"',/*manage_stock:*/'"0"',/*never_backorder:*/'"1"',/*use_config_manage_stock:*/'"0"',/*use_config_backorders:*/'"1"','"' + RTRIM(LTRIM(REPLACE(a.meta_title,'"','""'))) + '"'
-FROM dbo.tbl_LoadFile_SS13_OSP AS a
-WHERE type = 'configurable' AND vendor_product_id IN()
+		'"' + RTRIM(LTRIM(REPLACE(a.vendor_color_code,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.vendor_size_code,'"','""'))) + '"','"FW13 Inline"','"' + RTRIM(LTRIM(REPLACE(a. short_description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.description,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.features,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.activities,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weather,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.layering,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.care_instructions,'"','""'))) + '"',
+		'"' + RTRIM(LTRIM(REPLACE(a.fabric,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.fit,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.volume,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.manufacturer,'"','""'))) + '"',/*qty:*/'""',/*is_in_stock:*/'""','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.simples_skus,'FW13A','FW13I'),'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(REPLACE(a.url_key,'fw13a','fw13i'),'"','""'))) + '"',
+		'"' + RTRIM(LTRIM(REPLACE(a.videos,'"','""'))) + '"','"' + RTRIM(LTRIM(REPLACE(a.weight,'"','""'))) + '"',/*merchandise_priority:*/'"B"',/*backorders:*/'"0"',/*manage_stock:*/'"0"',/*never_backorder:*/'"1"',/*use_config_manage_stock:*/'"0"',/*use_config_backorders:*/'"1"','"' + RTRIM(LTRIM(REPLACE(a.meta_title,'"','""'))) + '"'
+FROM dbo.tbl_LoadFile_FW13_TNF AS a
+WHERE type = 'configurable' AND vendor_product_id IN('A0NF',
+'A0QB',
+'A2V2',
+'A7LN',
+'A7LP',
+'A7MP',
+'A7YK',
+'A8WB',
+'A8ZU',
+'AJGW',
+'AMVY',
+'AUDP',
+'AVDF')
 GO
 
 DECLARE @sql varchar(1024)
-SELECT @sql = 'bcp "SELECT * FROM LOT_Inventory.dbo.view_LoadFile_SS13_OSP" queryout "C:\Data\Shared\OSP-S13-1.csv" -w -t , -T -S ' + @@servername
+SELECT @sql = 'bcp "SELECT * FROM LOT_Inventory.dbo.view_Inline_PO_LoadFile" queryout "C:\Data\Shared\TNF-F13-1.csv" -w -t , -T -S ' + @@servername
 EXEC master..xp_cmdshell @sql
 
-DROP VIEW view_Inline_PO_LoadFile_LoadFile
-
-
+DROP VIEW view_Inline_PO_LoadFile
 
 
 

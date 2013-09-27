13c88a7b4f034b5106f64045e4a848f0272a0363
Tyler Gillson <ty@liveoutthere.com>
2013-09-27 13:52:11 -0600

    Updated LINTer & PO building procedure.

--- Tools/PO Building/LINT.php
+++ Tools/PO Building/LINT.php
@@ -434,7 +434,7 @@ class Linter
 							//if (count($missing_skus)) 
 							//	$configurable_errors[] = 'There are SKUs in the simples_skus value on row ' . ($row+1) . ' that are not in the loadfile: ' . implode(', ', $missing_skus);
 						
-							if (empty($data[Linter::FEATURES]) && $data[Linter::MANUFACTURER] != 'Icebreaker' || preg_match('/[^a-zA-Z0-9®!™%<>&";:()\/,|\.\-\' ]/',$data[Linter::FEATURES]) || substr_count($data[Linter::FEATURES],'||') > 0)
+							if (empty($data[Linter::FEATURES]) && $data[Linter::MANUFACTURER] != 'Icebreaker' || preg_match('/[^a-zA-Z0-9®!™%<>•&+";:()\/,|\.\-\' ]/',$data[Linter::FEATURES]) || substr_count($data[Linter::FEATURES],'||') > 0)
 								$configurable_errors[] = 'Check the features value on row ' . ($row+1) . '. It is either NULL, contains a special character we may not want, or has a double pipe.';// . $data[Linter::FEATURES];
 
 							if (!empty($data[Linter::FABRIC]) && preg_match('/[^a-zA-Z0-9®™²<>•+&%;:()\/,|\.\-\' ]/',$data[Linter::FABRIC]))

33fff3654241b4cc3e1d308f74df288a2519a787
Tyler Gillson <ty@liveoutthere.com>
2014-03-14 16:39:54 -0600

    Deleted a bunch of old useless tools / files

--- Tools/PO Building/Categorization Query.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT b.sku, dbo.getCategory(b.categories,'Patagonia',a.department) 
-FROM tbl_LoadFile_FW13_PAT AS a
-INNER JOIN tbl_FW13_Final_Categorization AS b
-ON a.sku = b.sku
-WHERE b.sku IN('PAT-28471')
